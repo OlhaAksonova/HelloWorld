f477aea245a0bf4eb7f5411c6dafe060f643700e
OlhaAksonova <olyaaksenova0883@gmail.com>
2022-06-28 14:17:12 +0300

    Hello java

---
+++ src/MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        if(a>b){
+            System.out.println("Число "+a+" больше "+b);
+        }else if (a<b){
+            System.out.println("Число "+a+" меньше "+b);
+        }else{
+            System.out.println("Число "+a+" равно "+b);
+        }
+    }
+}
